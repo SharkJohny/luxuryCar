1455a069dcfefec4e82f9c902c6009ce17623b72
Jan Kučera <93750764+SharkJohny@users.noreply.github.com>
2024-09-17 01:47:34 +0200

    A

--- assets/js/luxuryCar.js
+++ assets/js/luxuryCar.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
     intIndex()
     dinamicPictures()
     initHeader()
+    initModelSelect()
 });
 
 function intIndex() {
@@ -66,4 +67,93 @@ function initHeader() {
     $(
         '<a href="#" class="toggle-window" data-target="search" data-testid="linkSearchIcon"></a>'
     ).prependTo(".desktop  .navigation-buttons");
+}
+
+
+function initModelSelect() {
+    const section = $('<section>', {
+        id: 'model-selector'
+    }).insertAfter('.in-index .content-wrapper.container:eq(1)')
+    const container = $('<div>', {
+        class: 'model-selector container',
+    }).appendTo(section)
+    $('<h2>').text('Obchod pro tvoje Auto').appendTo(container)
+    const choiceWrap = $('<div>').addClass('modl-selector-wrap').appendTo(container)
+    const znacka = `
+        <div class="surcharge-list brands dm-selector">
+            <div class='label'>` + cstm_znacka.at(0) + `</div>
+            <div class='selector'>
+                <select required="required">
+                    <option class='notselect'>` + cstm_znacka.at(1) + `</option>
+                </select>
+            </div>
+        </div>
+        `
+
+    const model = `
+        <div class="surcharge-list models dm-selector">
+            <div class='label'>` + cstm_model.at(0) + `</div>
+            <div class='selector'>
+                <select required="required">
+                    <option class='notselect'>` + cstm_model.at(1) + `</option>
+                </select>
+            </div>
+        </div>
+        `
+
+    const rocnik = `
+        <div class="surcharge-list years dm-selector">
+            <div class='label'>` + cstm_rocnik.at(0) + `</div>
+            <div class='selector'>
+                <select required="required">
+                    <option class='notselect'>` + cstm_rocnik.at(1) + `</option>
+                </select>
+            </div>
+        </div>
+        `
+
+    const type = `
+        <div class="surcharge-list type dm-selector">
+            <div class='label'>` + cstm_type.at(0) + `</div>
+            <div class='selector'>
+                <select required="required">
+                    <option class='notselect'>` + cstm_type.at(1) + `</option>
+                </select>
+            </div>
+        </div>
+        `
+
+    $(znacka + model + rocnik + type).appendTo(choiceWrap)
+
+    for (let i = 0; i < types.length; i++) {
+        $("<option>" + types[i] + "</option>").appendTo(".type select")
+    }
+
+    const cars = car_json.cars;
+    const numberOfBrands = Object.keys(cars).length;
+    const brands = Object.keys(cars);
+    for (let i = 0; i < numberOfBrands; i++) {
+        $("<option>" + brands[i] + "</option>").appendTo(".brands select")
+    }
+    const d = new Date();
+    const currentYear = d.getFullYear();
+    for (let year = Number(years_from); year <= currentYear; year++) {
+        $("<option>" + year + "</option>").appendTo(".years select")
+    }
+
+    const other_option = "<option>" + other + "</option>"
+    $(other_option).appendTo(".type select")
+    $(other_option).appendTo(".years select")
+
+    $(".brands select").on("change", function() {
+        if ($(this).val() === cstm_znacka.at(1)) {
+            $(".models option:not(.notselect)").remove()
+        } else {
+            $(".models option:not(.notselect)").remove()
+            const models_for_brand = car_json.cars[$(this).val()];
+            for (let i = 0; i < models_for_brand.length; i++) {
+                $("<option>" + models_for_brand.at(i) + "</option>").appendTo(".models select")
+            }
+        }
+    })
 }
